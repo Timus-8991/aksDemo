63a8151aea6c0759f6d8427b00c13e851f239be0
Sumit Saini <sumit.saini@nagarro.com>
2019-06-25 15:51:37 +0530

    afdafrgcbcefa

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace aksDemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "inside aks demo", "gitops testing flux 4" };
+            return new string[] { "inside aks demo", "gitops testing flux 45" };
         }
 
         // GET api/values/5
