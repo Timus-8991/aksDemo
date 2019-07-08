5497a7154f684de69d89aef436b72277983dd726
SUMIT SAINI <sainisumit935@yahoo.com>
2019-07-08 10:07:12 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace aksDemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "vesrsioning", "test 1" };
+            return new string[] { "vesrsioning", "test 2" };
         }
 
         // GET api/values/5
